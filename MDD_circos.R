91d7c6d7f1b97ec074c7209e06b5c590346f3ff0
markdane <dane@ohsu.edu>
2019-05-01 05:52:07 -0700

    RPPA-only plots with line width and alpha based on positive correlations

--- MDD_circos.R
+++ MDD_circos.R
@@ -4,57 +4,28 @@ library(circlize)
 library(ComplexHeatmap)
 library(gridBase)
 
-load("MCF10A_RPPA_GCP_CYCIF_condition_correlation.Rdata")
-
-prepare_data <- function(x){
-  df <- x %>%
-    data.frame() %>%
-    rownames_to_column(var ="Condition") %>%
-    mutate(Time = str_remove(Condition, ".*_"),
-           Time = str_replace_all(Time, "^4$","1"),
-           Time = str_replace_all(Time, "^8$","2"),
-           Time = str_replace_all(Time, "^24$","3"),
-           Time = str_replace_all(Time, "^48$","4"),
-           Ligand = str_remove(Condition, "_.*"))
-}
-
-col_names <- c("grey50", "grey25","red3","royalblue3")
-
-rppa <- prepare_data(cormats[["RPPA"]])
-cycIF <- prepare_data(cormats[["CYCIF"]])
-GCP <- prepare_data(cormats[["GCP"]])
-
-circos.par("track.height" = 0.2)
-circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
-
-circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
-  xlim = CELL_META$xlim
-  ylim = CELL_META$ylim
-  breaks = seq(xlim[1], xlim[2], by = 1)
-  n_breaks = length(breaks)
-  circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
-              breaks[-1], rep(ylim[2], n_breaks - 1),
-              col = col_names,
-              border = NA)
-  circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
-              CELL_META$sector.index)
-}) 
 parseCorMatrix <- function(x){
+  alpha_factor <- 128
   from_condition <- x$Condition
   to_condition <- names(x)[c(-1,-22,-23)]
-  alpha <- x[c(-1,-22,-23)] %>%
+  #remove metadata from correlations
+  cors <- x[c(-1,-22,-23)] %>%
     unlist()
-  alpha <- (alpha * 16) %% 256
+  #remove correlations within the ligand conditions
+  cors[str_detect(names(cors), x[["Ligand"]])] <- 0
+  #scale alpha based on correlations
+  alpha <- (cors * alpha_factor) %% 256
   alpha <- as.integer(alpha) %>%
     as.character.hexmode()
-  col <- paste0("#000000",alpha)
+  col <- paste0(col_ligands[x$Ligand],alpha)
   df <- tibble(From_Condition = x$Condition,
-                    To_Condition =  names(x)[c(-1,-22,-23)],
-                    From_sector = str_remove(from_condition, "_.*"),
-                    To_sector = str_remove(to_condition, "_.*"),
-                    From_timepoint = str_remove(from_condition, ".*_"),
-                    To_timepoint = str_remove(to_condition, ".*_"),
-                    Col = col) %>%
+               To_Condition =  names(x)[c(-1,-22,-23)],
+               From_sector = str_remove(from_condition, "_.*"),
+               To_sector = str_remove(to_condition, "_.*"),
+               From_timepoint = str_remove(from_condition, ".*_"),
+               To_timepoint = str_remove(to_condition, ".*_"),
+               Col = col,
+               Cors = as.numeric(cors)) %>%
     mutate(From_timepoint = str_replace(From_timepoint,"^4$",".5"),
            From_timepoint = str_replace(From_timepoint,"^8$","1.5"),
            From_timepoint = str_replace(From_timepoint,"^24$","2.5"),
@@ -71,75 +42,162 @@ parseCorMatrix <- function(x){
            To_timepoint_end = To_timepoint + .5)
 }
 
+circlize_plot <- function() {
+  circos.initializeWithIdeogram(plotType = NULL)
+  
+  bed = generateRandomBed(nr = 300)
+  bed = generateRandomBed(nr = 300, nc = 2)
+  circos.genomicTrackPlotRegion(bed,
+                                panel.fun = function(region, value, ...) {
+                                  circos.genomicPoints(region, value, cex = 0.5, pch = 16, col = 2:3, ...)
+                                })
+  
+  bed = generateRandomBed(nr = 500, nc = 2)
+  circos.genomicTrackPlotRegion(bed,
+                                panel.fun = function(region, value, ...) {
+                                  circos.genomicLines(region, value, col = 4:5, ...)
+                                })
+  
+  bed1 = generateRandomBed(nr = 100)
+  bed1 = bed1[sample(nrow(bed1), 20), ]
+  bed2 = generateRandomBed(nr = 100)
+  bed2 = bed2[sample(nrow(bed2), 20), ]
+  
+  circos.genomicLink(bed1, bed2, col = col_fun(bed1[[4]]))
+  
+  circos.clear()
+}
 addLink <- function(x){
-  # circos.link(x["From_sector"], c(as.numeric(x["From_timepoint_st"]), as.numeric(x["From_timepoint_end"])), x["To_sector"], c(as.numeric(x["To_timepoint_st"]), as.numeric(x["To_timepoint_end"])), h = 0.4, lwd = .01, col =x["Col"])
-  circos.link(x["From_sector"], as.numeric(x["From_timepoint"]), x["To_sector"], as.numeric(x["To_timepoint"]), h = 0.4, lwd = 1, col =x["Col"])
+  cor_thresh <- .4
+  if(x["Cors"] > cor_thresh) {
+    lwd <- max(0, as.numeric(x["Cors"])-cor_thresh)*30
+    #cat("lwd ",lwd, "col ", x["Col"])
+    circos.link(x["From_sector"], as.numeric(x["From_timepoint"]), x["To_sector"], as.numeric(x["To_timepoint"]), h = 0.4, lwd = lwd, col =x["Col"])
+  }
 }
 
-plot.new()
-circle_size = unit(1, "snpc")
-pushViewport(viewport(x = 0.5, y = 1, width = circle_size, height = circle_size,
-                      just = c("center", "top")))
-par(omi = gridOMI(), new = TRUE)
-circlize_plot()
-upViewport()
+prepare_data <- function(x){
+  df <- x %>%
+    data.frame() %>%
+    rownames_to_column(var ="Condition") %>%
+    mutate(Time = str_remove(Condition, ".*_"),
+           Time = str_replace_all(Time, "^4$","1"),
+           Time = str_replace_all(Time, "^8$","2"),
+           Time = str_replace_all(Time, "^24$","3"),
+           Time = str_replace_all(Time, "^48$","4"),
+           Ligand = str_remove(Condition, "_.*"))
+}
 
-draw(lgd_list, y = unit(1, "npc") - circle_size, just = "top")
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
 
-title("RPPA Correlations")
+######end of functions
 
-for(i in 1:20){
+load("MCF10A_RPPA_GCP_CYCIF_condition_correlation.Rdata")
+
+
+
+rppa <- prepare_data(cormats[["RPPA"]])
+cycIF <- prepare_data(cormats[["CYCIF"]])
+GCP <- prepare_data(cormats[["GCP"]])
+
+#Assign ggplot colors to the ligands based on their order in the RPPA dataset
+col_ligands <- gg_color_hue(5)
+names(col_ligands) <- unique(rppa$Ligand)
+#Assign colors to conditions in the track
+col_names <- c("grey50", "grey25","red3","royalblue3")
+
+draw_one_track <- function(title_text){
+  circos.par("track.height" = 0.2)
+  circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
+  
+  circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
+    xlim = CELL_META$xlim
+    ylim = CELL_META$ylim
+    breaks = seq(xlim[1], xlim[2], by = 1)
+    n_breaks = length(breaks)
+    circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
+                breaks[-1], rep(ylim[2], n_breaks - 1),
+                col = col_names,
+                border = NA)
+    circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
+                CELL_META$sector.index)
+  }) 
+  
+  
+  title(title_text)
+  
+}
+
+res <- draw_one_track(title_text = "RPPA Correlations")
+
+#for(i in 1:20){
+for(i in 1){
   foo <- parseCorMatrix(rppa[i,])
   res <- apply(foo, 1, addLink)
 }
 
-circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
-# create legend
-lgd = Legend(at = c(4,8,24,48), type = "grid", 
-             legend_gp = gpar(col = 4:5, lwd = 2),
-             title_position = "topleft",
-             title = "Time")
-lgd_list <- packLegend(lgd)
-
-circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
-  xlim = CELL_META$xlim
-  ylim = CELL_META$ylim
-  breaks = seq(xlim[1], xlim[2], by = 1)
-  n_breaks = length(breaks)
-  circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
-              breaks[-1], rep(ylim[2], n_breaks - 1),
-              col = col_names,
-              border = NA)
-  circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
-              CELL_META$sector.index)
-}) 
-
-title("cycIF Correlations")
-
-for(i in 1:20){
-  foo <- parseCorMatrix(cycIF[i,])
+pdf("RPPA Circoses.pdf",width = 4, height = 4)
+for(ligand in unique(rppa$Ligand)){
+  res <- draw_one_track(title_text = "RPPA Correlations")
+  df <- rppa %>%
+    filter(Ligand == ligand)
+  for(i in 1:nrow(df)){
+    foo <- parseCorMatrix(df[i,])
+    res <- apply(foo, 1, addLink)
+  }
+}
+res <- draw_one_track(title_text = "RPPA Correalations")
+df <- rppa
+for(i in 1:nrow(df)){
+  foo <- parseCorMatrix(df[i,])
   res <- apply(foo, 1, addLink)
 }
-
-circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
-
-circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
-  xlim = CELL_META$xlim
-  ylim = CELL_META$ylim
-  breaks = seq(xlim[1], xlim[2], by = 1)
-  n_breaks = length(breaks)
-  circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
-              breaks[-1], rep(ylim[2], n_breaks - 1),
-              col = col_names,
-              border = NA)
-  circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
-              CELL_META$sector.index)
-}) 
-
-draw(lgd_list, x = unit(4, "mm"), y = unit(4, "mm"), just = c("left", "bottom"))
-title("GCP Correlations")
-
-for(i in 1:20){
-  foo <- parseCorMatrix(GCP[i,])
-  res <- apply(foo, 1, addLink)
-}
+dev.off()
+# 
+# circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
+# 
+# circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
+#   xlim = CELL_META$xlim
+#   ylim = CELL_META$ylim
+#   breaks = seq(xlim[1], xlim[2], by = 1)
+#   n_breaks = length(breaks)
+#   circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
+#               breaks[-1], rep(ylim[2], n_breaks - 1),
+#               col = col_names,
+#               border = NA)
+#   circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
+#               CELL_META$sector.index)
+# }) 
+# 
+# title("cycIF Correlations")
+# 
+# for(i in 1:20){
+#   foo <- parseCorMatrix(cycIF[i,])
+#   res <- apply(foo, 1, addLink)
+# }
+# 
+# circos.initialize(factors = as.factor(rppa$Ligand), xlim =c(0,4), sector.width = 20)
+# 
+# circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
+#   xlim = CELL_META$xlim
+#   ylim = CELL_META$ylim
+#   breaks = seq(xlim[1], xlim[2], by = 1)
+#   n_breaks = length(breaks)
+#   circos.rect(breaks[-n_breaks], rep(ylim[1], n_breaks - 1),
+#               breaks[-1], rep(ylim[2], n_breaks - 1),
+#               col = col_names,
+#               border = NA)
+#   circos.text(CELL_META$xcenter, CELL_META$cell.ylim[2] + uy(5, "mm"),
+#               CELL_META$sector.index)
+# }) 
+# 
+# #draw(lgd_list, x = unit(4, "mm"), y = unit(4, "mm"), just = c("left", "bottom"))
+# title("GCP Correlations")
+# 
+# for(i in 1:20){
+#   foo <- parseCorMatrix(GCP[i,])
+#   res <- apply(foo, 1, addLink)
+# }
