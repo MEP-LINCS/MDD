bb482a36e380aee9304732732237029fafad9db2
Mark Dane <you@example.com>
2019-05-23 10:43:43 -0700

    Clean up for release

--- R/mdd_preprocess_rppa.R
+++ R/mdd_preprocess_rppa.R
@@ -65,16 +65,8 @@ CV <- function(x){
 }
 
 
-# The RPPA data is delivered in a multi-sheet excel file. This vignette starts by loading the header and data from the NormLinear sheet. 
-
-# #temporary function for Ligand_2 metadata
-#   label_ligand_2 <- function(x) {
-#     ligand_2 <- rep("None", times = length(x))
-#     ligand_2[x %in% c("BMP2", "IFNG", "TGFB")] <- "EGF"
-#     return(ligand_2)
-#   }
-#   
-l2Header <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), readRPPAHeader) %>%
+#Loading the header and data from the raaw data NormLinear sheet. 
+l1Header <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), readRPPAHeader) %>%
   bind_rows() %>%
   spread(ReplicateSet,`Probabilities (QC Score)`) %>%
   mutate(SetAQCScore=signif(as.numeric(A),2),
@@ -83,7 +75,7 @@ l2Header <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), read
   rename(Antibody=`Antibody Name in Heatmap`) %>%
   select(-A, -B, -C)
 
-l2_data <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), readRPPAData) %>%
+l1_data <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), readRPPAData) %>%
   bind_rows() %>%
   select(-matches("Order|Sample [(Source)(Name)(Type)]|Category_[23]|CF1&2 |^CF[12]$")) %>%
   gather(key = "antibody",value = "value",-Category_1, -`Sample description`) %>%
@@ -103,47 +95,23 @@ l2_data <- lapply(dir("../RPPA/Data", pattern = "01_", full.names = TRUE), readR
 
 mddMetadata <- read_csv("../Metadata/MDD_sample_annotations.csv")
 
-l2 <- l2_data %>%
+l1 <- l1_data %>%
   left_join(mddMetadata, by = "specimenName")
 
-l2_synapse<- l2 %>%
+l1_synapse<- l1 %>%
   select(specimenID, antibody, value) %>%
   spread(key = specimenID, value = value) %>%
   select(antibody, str_sort(colnames(.), numeric=TRUE)) %>%
   write_csv("../RPPA/Data/MDD_RPPA_level1.csv")
 
 
-
-# Next we will add metadata about the antibodies.
-
-
-RPPAMetadata <- read_csv("../RPPA/Metadata/RPPA_Metadata.csv",col_types = cols(
-  MDD = col_character(),
-  Symbols = col_character(),
-  Sites = col_character(),
-  Effect = col_character(),
-  Pathway = col_character(),
-  Protein = col_character(),
-  Direction = col_integer(),
-  RTK = col_logical(),
-  IncycIF = col_logical(),
-  Fig1 = col_logical(),
-  Histone = col_logical(),
-  CellCycle = col_logical()
-)) %>%
-  mutate(MDD = str_remove(MDD,"-.-.$"))
-
-
-
 # The RPPA data is time course normalized by dividing each antibody by the corresponding time course values of EGF.  
-
-
-egf <- l2 %>%
+egf <- l1 %>%
   filter(ligand=="EGF") %>%
   select(antibody,replicate,value,experimentalTimePoint) %>%
   rename(EGFValue=value)
 
-l4 <- l2 %>%
+l4 <- l1 %>%
   left_join(egf,by=c("antibody","replicate","experimentalTimePoint"))
 
 l4 <- l4 %>%
@@ -155,43 +123,3 @@ l4_synapse<- l4 %>%
   spread(key = specimenID, value = value) %>%
   select(antibody, str_sort(colnames(.), numeric=TRUE),-sid1, -sid2, -sid3) %>%
   write_csv("../RPPA/Data/MDD_RPPA_level4.csv")
-
-
-
-
-# The data is from three biological replicate experiments. Next, the replicates are summarized to the condition (ligand+time point) level.
- 
-
-###Data Descriptions
-# **SampleDescription** - The experimental condition in the format ligand_timePoint\_replicate  
-# **ProteinConcentration** - The protein concentration in the sample sent to MDACC  
-# **Replicate** - The experiment includes triplicate samples gathered over three different weeks. `Replicate` values are only present in the Level 2 data. The Level 3 data is summarized across the replicates.  
-# **CV** - Coefficient of variation of the replicates. Level 3 only.  
-# **ValueVariance** - The variance of the time course values within a ligand treatment.  
-# **Antibody** - The MDACC name of the antibody.  
-# **Value** - The MDACC normalized linear value.  
-# **Ligand** - The cells were incubated with one of six ligands or PBS. The time 0 samples have a ligand value of "ctrl".    
-# **TimePoint** - Incubation time in hours. Values are 0, 1, 4, 8, 24 and 48.  
-# **Ligand_TimePoint** - Concatenation of Ligand and TimePoint values with "_" separator.    
-# **Symbols** - HUGO symbol for the protein associated with the antibody.   
-# **Sites** - Phosphorylation site. Multiple sites are separated by a pipe symbol. If there are multiple symbols (genes) the phosphorylated sites for each will be separated by a space.  
-# **Effect** - Used for CausalPath analysis    
-# **Pathway** - Manually curated pathway assignment.    
-# **Protein** - Redundant annotation to be depracated.   
-# **Direction** - Used in pathway analysis.  
-# **RTK** - Logical value for whether the antibody detects a Receptor Tyrosine Kinase.  
-# **IncycIF** -  Logical value for whether the antibody overlaps with the cycIF antibodies.  
-# **Fig1** -  Logical value for whether the antibody is included in figure 1. 
-# **Antibody Origin** - MDACC annotation.  
-# **Gene Name** - MDACC annotation.   
-# **Validation Status** - MDACC annotation.  
-# **Slide Id** - MDACC annotation.  
-# **Slide no.** - MDACC annotation.   
-# **Sample Type** - MDACC annotation.    
-# **SetAQCScore** - MDACC QC value.  
-# **SetBQCScore** - MDACC QC value.    
-# **SetCQCScore** - MDACC QC value.   
-# **T0Norm** - Time 0 normalized value calculated by dividing `Value` by the `Value` of `ctrl_0` of the same antibody in its replicate set.    
-# **TCNorm**  - Time course normalized value calculated by dividing `Value` by either the PBS or EGF `Value` of the same antibody and same time point in its replicate set. BMP2, IFNG, and TGFB samples were incubated in the presence of EGF and are normalized against the EGF values. EGF, HGF and OSM samples were incubated in PBS and are normalized against their corresponding PBS values.  
-# **PBSNorm**  - Time course normalized value calculated by dividing `Value` by the PBS `Value` of the same antibody and same time point in its replicate set.  
-# **EGFNorm**  - Time course normalized value calculated by dividing `Value` by the egf `Value` of the same antibody and same time point in its replicate set.  
