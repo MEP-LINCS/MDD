00772549b63ccf0ca054897a19ff782bd6f2156c
Mark Dane <you@example.com>
2019-06-10 15:35:38 -0700

    using new cycIF data

--- R/MDD_cycIF_preprocess.R
+++ R/MDD_cycIF_preprocess.R
@@ -13,6 +13,16 @@ cycIF_Data <- read_csv("../cycIF/Data/biological_replicate_means.csv") %>%
   filter(!str_detect(feature, "background")) %>%
   write_csv("../cycIF/Data/MDD_cycIF_Level3.csv")
 
+cycIF_Data_archive <- read_csv("../cycIF/Data/biological_replicate_means(archive).csv") %>%
+  mutate(condition = str_remove(X1, "_[ABCDE]"),
+         specimenName = paste0(condition, "_C3_",str_remove(X1,".*_"))) %>%
+  select(-X1, -condition) %>%
+  gather(key = feature, value = value, -specimenName) %>%
+  inner_join(mddMetadata, by = "specimenName") %>%
+  select(specimenID, feature, value) %>%
+  spread(specimenID, value) %>%
+  filter(!str_detect(feature, "background"))
+
 #get EGF values for each feature within each time point and replicate
 egf <- cycIF_Data %>%
   gather(specimenID, value, -feature) %>%
