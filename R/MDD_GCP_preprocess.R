b79a0cec4bf5f775e0f139a341b90a6796338978
Mark Dane <you@example.com>
2019-08-06 10:34:14 -0700

    redefine level 2 as level 3 median-summarize for level 4

--- R/MDD_GCP_preprocess.R
+++ R/MDD_GCP_preprocess.R
@@ -10,7 +10,7 @@ GCP_Data <- read_tsv("../GCP/Data/GCP_MCF10a_log2_noProbeMetadata.txt", skip = 2
   as_tibble()
 colnames(GCP_Data) <- GCP_Data[1,]
 
-l2_long <- GCP_Data %>%
+l3_long <- GCP_Data %>%
   slice(-1) %>%
   filter(!is.na(id)) %>%
   mutate(ligand = pert_iname,
@@ -27,39 +27,20 @@ l2_long <- GCP_Data %>%
   select(specimenID, matches("^H3K|H4")) %>%
   gather(key = histone, value = value, -specimenID)
 
-l2 <- l2_long %>%
+l3 <- l3_long %>%
   select(specimenID, histone, value) %>%
   spread(key = specimenID, value = value) %>%
-  mutate(histone = factor(histone, levels = unique(l2_long$histone))) %>%
+  mutate(histone = factor(histone, levels = unique(l3_long$histone))) %>%
   arrange(histone) %>%
-  write_csv("../GCP/Data/MDD_GCP_Level2.csv")
+  write_csv("../GCP/Data/MDD_GCP_Level3.csv")
 
-#create EGF normalized level 4 data
-#Reformat the file from the Broad to match the MDD format
-#Select the histone names and sample identifiers then
-#use the 20th row as the column names
-GCP_level4_file <- read_tsv("../GCP/Data/MCF10a_EGFtimepointnorm_RepRemoved_withPBS_OHSU_metadata.txt", skip = 2) 
-GCP_level4_file[(GCP_level4_file$id == "sampleid"),] <- paste(GCP_level4_file[(GCP_level4_file$id == "condition"),],
-                                                              GCP_level4_file[(GCP_level4_file$id == "collection"),],
-                                                              GCP_level4_file[(GCP_level4_file$id == "replicate"),],
-                                                              sep="_")
-GCP_level4_file <- GCP_level4_file %>%
-  select(matches("histone|GMa")) %>%
-  rename(histone = pr_gcp_histone_mark)
-
-colnames(GCP_level4_file)[2:ncol(GCP_level4_file)] <- GCP_level4_file[20,2:ncol(GCP_level4_file)] 
-
-#Delete the first 23 rows since they contain unneeded metadata
-#match with MDD metadata file to use specimenID values
-l4 <- slice(GCP_level4_file, 24:nrow(GCP_level4_file)) %>%
-  gather(specimenName, value = value, -histone) %>%
-  mutate(specimenName = str_replace(specimenName,"IFNg","IFNG"),
-         specimenName = str_replace(specimenName,"TGFb","TGFB")) %>%
-  inner_join(mddMetadata, by = "specimenName") %>%
-  select(specimenID, histone, value) %>%
-  mutate(value = as.numeric(value)) %>%
-  spread(key = specimenID, value = value) %>%
-  mutate(histone = factor(histone, levels = unique(GCP_level4_file$histone))) %>%
-  arrange(histone) %>%
+#median summarize replicates by condition
+l4 <- l3 %>% 
+  gather(specimenID, value, -histone) %>%
+  left_join(mddMetadata, by = "specimenID") %>%
+  select(experimentalCondition, value, histone) %>%
+  group_by(experimentalCondition, histone) %>%
+  summarise(value = median(value, na.rm = TRUE)) %>%
+  ungroup() %>%
+  spread(key = experimentalCondition, value = value) %>%
   write_csv("../GCP/Data/MDD_GCP_Level4.csv")
-
