6272f122bf2bb7d16e9de22026102e4b161d0a96
Mark Dane <dane@ohsu.edu>
2020-07-31 11:08:52 -0700

    started generalization

--- R/MDD_RPPA_limma.R
+++ R/MDD_RPPA_limma.R
@@ -10,15 +10,14 @@ library(tidyverse)
 library(limma)
 library(ComplexHeatmap)
 
+assays <- c("RPPA", "GCP")
 
-RPPAlevel3File <- "../RPPA/Data/MDD_RPPA_Level3.csv"
-MDDannoFile <- "../Metadata/MDD_sample_annotations.csv"
 
 logFC_threshold <- 0.25
 pval_threshold  <- 0.01
 
-outDirPlots <- "../plots/RPPA_limma"
-outDirData <- "../RPPA/Data/DEResults"
+outDirPlots <- paste0("../plots/",assay,"_limma")
+outDirData <- paste0("../",assay,"/Data/DEResults")
 
 if (!dir.exists(outDirPlots)) {
   dir.create(outDirPlots)
@@ -28,53 +27,70 @@ if (!dir.exists(outDirData)) {
   dir.create(outDirData)
 }
 
-###############################################################################
+get_assay_values <- function(assay){
+  level3File <- paste0("../",assay,"/Data/MDD_",assay,"_Level3.csv")
+  MDDannoFile <- "../Metadata/MDD_sample_annotations.csv"
+  
+  mat <- read.table(level3File,
+                    header = TRUE,
+                    sep = ",",
+                    row.names = 1)
+  
+  meta <- read.table(MDDannoFile,
+                     header = TRUE,
+                     sep = ",") %>% 
+    filter(str_detect(experimentalTimePoint, "0|24|48"),
+           specimenID %in% colnames(mat)) %>% 
+    dplyr::select(specimenID, specimenName, ligand, experimentalTimePoint, experimentalCondition, replicate) %>% 
+    mutate(experimentalCondition = fct_inorder(factor(experimentalCondition)))
+  
+  mat <- mat %>%
+    dplyr::select(all_of(unique(meta$specimenID)))
+  return(list(mat = mat, meta = meta))
+}
 
-RPPA.mat <- read.table(RPPAlevel3File,
-                       header = TRUE,
-                       sep = ",",
-                       row.names = 1)
-RPPA.meta <- read.table(MDDannoFile,
-                        header = TRUE,
-                        sep = ",") %>% 
-  filter(str_detect(experimentalTimePoint, "0|24|48"),
-         specimenID %in% colnames(RPPA.mat)) %>% 
-  dplyr::select(specimenID, specimenName, ligand, experimentalTimePoint, experimentalCondition, replicate) %>% 
-  mutate(experimentalCondition = fct_inorder(factor(experimentalCondition)))
-
-RPPA.mat <- RPPA.mat %>%
-  dplyr::select(all_of(unique(RPPA.meta$specimenID)))
 
 ###############################################################################
 # Creating design for comparisons.
 # All conditions to be compared to ctrl_0.
 #limma anlaysis expects log-expression values
-design <- model.matrix(~experimentalCondition, RPPA.meta)
 
-lm <- lmFit(RPPA.mat, design)
-lm <- treat(lm, lfc = logFC_threshold)
+foo <- lapply(assays, function(assay){
+  
+  assay_values <- get_assay_values(assay)
+  
+  design <- model.matrix(~experimentalCondition, assay_values[["meta"]])
+  
+  lm <- lmFit(assay_values[["mat"]], design)
+  lm <- treat(lm, lfc = logFC_threshold)
+  
+  #Filter to significant condition rows
+  sig_conditions <- decideTests(lm, p.value = pval_threshold)
+  #Filter to non-zero condition rows
+  sig_conditions <- sig_conditions@.Data[,-1]
+  feature_index <- !rowSums(abs(sig_conditions)) == 0
+  features <- rownames(sig_conditions)[feature_index]
+  
+ lfc_values <- lm[["coefficients"]][features,]
+  p_values <- lm[['p.value']][features,]
 
-res <- decideTests(lm, p.value = pval_threshold)
-
-lfc_values <- lm[["coefficients"]]
-p_values <- lm[['p.value']]
-tt <- topTreat(lm, number = Inf)
-
-write.csv(tt, sprintf("%s/RPPA_DE_topTreat.csv", outDirData))
-
-resTP <- matrix(res, nrow = nrow(res))[,-1]
-rownames(resTP) <- rownames(res)
-colnames(resTP) <- colnames(design)[-1] %>%
-  str_remove("experimentalCondition")
-
-
-pdf(sprintf("%s/RPPA_significantAnalytes.pdf", outDirPlots), height = 12, width = 16)
-hm <- Heatmap(resTP,
-              name = "significant",
-              cluster_columns = FALSE,
-              show_column_names = TRUE,
-              column_names_gp = gpar(fontsize = 9),
-              show_row_names = TRUE,
-              column_title = "RPPA: significant histone epigentic marks compared to ctrl_0")
-draw(hm)
-dev.off()
+  write.csv(lfc_values, sprintf("%s/%s_DE_lfc_values.csv", outDirData, assay))
+  write.csv(p_values, sprintf("%s/%s_DE_p_values.csv", outDirData, assay))
+  
+  resTP <- matrix(res, nrow = nrow(res))[,-1]
+  rownames(resTP) <- rownames(res)
+  colnames(resTP) <- colnames(design)[-1] %>%
+    str_remove("experimentalCondition")
+  
+  pdf(sprintf("%s/%s_significantAnalytes.pdf", outDirPlots, assay), height = 12, width = 16)
+  hm <- Heatmap(resTP,
+                name = "significant",
+                cluster_columns = FALSE,
+                show_column_names = TRUE,
+                column_names_gp = gpar(fontsize = 9),
+                show_row_names = TRUE,
+                column_title = paste0(assay, ": significant features compared to ctrl_0"))
+  draw(hm)
+  dev.off()
+  
+})
