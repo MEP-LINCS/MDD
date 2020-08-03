ca6a4db523cb17c59a32c1158a650965557e65d4
Mark Dane <dane@ohsu.edu>
2020-08-03 16:09:57 -0700

    Using lfc data directly from fit object

--- R/MDD_build_integrated_matrix.R
+++ R/MDD_build_integrated_matrix.R
@@ -5,20 +5,19 @@ library(tidyverse)
 assays <- c("GCP", "cycIF", "motifs", "RPPA", "RNAseq")
 
 read_tt <- function(x){
-  
+  #browser()
   if(x == "motifs"){
     data_type = "ATACseq"
   } else {
     data_type = x
   }
-  foo <- paste0("../", data_type, "/Data/DEResults/",x,"_DE_topTables.csv") %>% 
+  foo <- paste0("../", data_type, "/Data/DEResults/",x,"_DE_lfc_values.csv") %>% 
     read_csv
 }
 T0_DE_integrated <- map(assays, read_tt) %>%
   bind_rows(.id = "Type") %>%
   mutate(Type = assays[as.integer(Type)]) %>%
   rename(feature = X1) %>%
-  rename_with( ~gsub("experimentalCondition", "", .x, fixed = TRUE)) %>%
-  filter(!AveExpr == 0)
+  rename_with( ~gsub("experimentalCondition", "", .x, fixed = TRUE))
 
 write_csv(T0_DE_integrated, "../integrated_analysis/integrated_T0_DE.csv")
