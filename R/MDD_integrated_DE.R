cd3177ea0bfe04f3eb304cc374541fb1226d2a2f
Mark Dane <dane@ohsu.edu>
2020-08-03 16:09:13 -0700

    Keeping ensembl ids Converting motifs to z scores Deleting all 0 rows

--- R/MDD_integrated_DE.R
+++ R/MDD_integrated_DE.R
@@ -6,27 +6,30 @@
 # L2FCs and or DE proteins
 
 library(tidyverse)
-#library(cowplot)
+library(biomaRt)
 library(limma)
 library(ComplexHeatmap)
 
 assays <- c("cycIF","RPPA", "GCP", "motifs", "RNAseq")
-assays <- c("motifs")
+assay <- c("RNAseq")
 
 logFC_threshold <- 0.25
 pval_threshold  <- 0.01
 
 
 get_assay_values <- function(assay){
+  #browser()
   level3File <- paste0("../",assay,"/Data/MDD_",assay,"_Level3.csv")
   if(assay == "motifs") level3File <- "../ATACseq/Data/MDD_ATACseq_MotifFamilyScores.csv"
   MDDannoFile <- "../Metadata/MDD_sample_annotations.csv"
   
   mat_raw <- read.table(level3File,
-                    header = TRUE,
-                    sep = ",",
-                    row.names = 1)
-  mat <- mat_raw[complete.cases(mat_raw),]
+                        header = TRUE,
+                        sep = ",",
+                        row.names = 1)
+  row_sd <- apply(mat_raw, 1, sd, na.rm = TRUE)
+
+  mat <- mat_raw[complete.cases(mat_raw) &!row_sd == 0,]
   
   meta <- read.table(MDDannoFile,
                      header = TRUE,
@@ -40,8 +43,31 @@ get_assay_values <- function(assay){
     mat <- mat[!str_detect(rownames(mat),"_med_|dna.*cytoplasm|laws|nucrin|centcyto|plasmem|none"),]
     mat <- log2(mat+.0001)
   }
-
   
+  if(assay == "motifs"){
+    mat_sd <- sd(unlist(mat), na.rm = TRUE)
+    mat <- mat/mat_sd
+  }
+  
+  if(assay == "RNAseq"){
+    #Get annotations to convert from ensemble to HGNC
+    mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                    dataset = "hsapiens_gene_ensembl",
+                    host = "uswest.ensembl.org")
+    
+    annoTable <- getBM(attributes = c("ensembl_gene_id",
+                                      "hgnc_symbol"),
+                       mart = mart)
+    mat <- mat %>%
+      rownames_to_column("ensembl_gene_id") %>%
+      # left_join(annoTable, by = "ensembl_gene_id") %>%
+      # dplyr::select(-ensembl_gene_id) %>%
+      group_by(ensembl_gene_id) %>%
+      summarise(across(everything(),mean), .groups = "drop") %>%
+      data.frame()
+rownames(mat) <- mat$ensembl_gene_id
+
+  }
   
   mat <- mat %>%
     dplyr::select(all_of(unique(meta$specimenID)))
@@ -55,6 +81,7 @@ get_assay_values <- function(assay){
 #limma anlaysis expects log-expression values
 
 foo <- lapply(assays, function(assay){
+ #browser() 
   assay_values <- get_assay_values(assay)
   
   design <- model.matrix(~experimentalCondition, assay_values[["meta"]])
@@ -66,13 +93,13 @@ foo <- lapply(assays, function(assay){
   sig_conditions <- decideTests(lm, p.value = pval_threshold)
   #Filter to non-zero condition rows
   sig_conditions <- sig_conditions@.Data[,-1]
-    colnames(sig_conditions) <- str_remove(colnames(sig_conditions), "experimentalCondition") 
+  colnames(sig_conditions) <- str_remove(colnames(sig_conditions), "experimentalCondition") 
   feature_index <- !rowSums(abs(sig_conditions)) == 0
-  features <- rownames(sig_conditions)[feature_index]
+  features <- rownames(sig_conditions)[feature_index] 
   
- lfc_values <- lm[["coefficients"]][features,]
+  lfc_values <- lm[["coefficients"]][features,]
   p_values <- lm[['p.value']][features,]
-
+  
   
   outDirPlots <- paste0("../plots/",assay,"_limma")
   outDirData <- paste0("../",assay,"/Data/DEResults")
