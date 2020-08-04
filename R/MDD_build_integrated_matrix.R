ffe20ff5504c3ddfc938b850b4a922f9cf1183fa
Mark Dane <dane@ohsu.edu>
2020-08-03 17:31:31 -0700

    Write out adjusted p values from all assays

--- R/MDD_build_integrated_matrix.R
+++ R/MDD_build_integrated_matrix.R
@@ -4,20 +4,32 @@ library(tidyverse)
 
 assays <- c("GCP", "cycIF", "motifs", "RPPA", "RNAseq")
 
-read_tt <- function(x){
+read_assay_values <- function(x, output_type = "lfc_values"){
   #browser()
   if(x == "motifs"){
     data_type = "ATACseq"
   } else {
     data_type = x
   }
-  foo <- paste0("../", data_type, "/Data/DEResults/",x,"_DE_lfc_values.csv") %>% 
+  foo <- paste0("../", data_type, "/Data/DEResults/",x,"_DE_",output_type,".csv") %>% 
     read_csv
 }
-T0_DE_integrated <- map(assays, read_tt) %>%
+
+output_type =  "lfc_values"
+T0_DE_integrated <- map(assays, read_assay_values, output_type) %>%
   bind_rows(.id = "Type") %>%
   mutate(Type = assays[as.integer(Type)]) %>%
+  mutate(across(where(is.numeric), signif, digits = 4)) %>%
   rename(feature = X1) %>%
   rename_with( ~gsub("experimentalCondition", "", .x, fixed = TRUE))
 
-write_csv(T0_DE_integrated, "../integrated_analysis/integrated_T0_DE.csv")
+write_csv(T0_DE_integrated, paste0("../integrated_analysis/integrated_T0_DE_",output_type,".csv"))
+
+output_type =  "adj_p_values"
+T0_DE_integrated <- map(assays, read_assay_values, output_type) %>%
+  bind_rows(.id = "Type") %>%
+  mutate(Type = assays[as.integer(Type)]) %>%
+  mutate(across(where(is.numeric), signif, digits = 4))
+
+write_csv(T0_DE_integrated, paste0("../integrated_analysis/integrated_T0_DE_",output_type,".csv"))
+
