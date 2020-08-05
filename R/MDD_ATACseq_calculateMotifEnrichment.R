11c6d647545e954afe112dfbe4f546ef4b390b5c
Daniel Derrick <derrickd@ohsu.edu>
2020-08-05 15:36:01 -0700

    scripts for calculating motif enrichment scores

---
+++ R/MDD_ATACseq_calculateMotifEnrichment.R
@@ -0,0 +1,120 @@
+library(tidyverse)
+library(DiffBind)
+library(motifmatchr)
+library(chromVAR)
+library(chromVARmotifs)
+library(Matrix)
+library(SummarizedExperiment)
+library(BSgenome.Hsapiens.UCSC.hg38)
+
+set.seed(2019)
+
+rdata     <- "/Users/derrickd/workspace/mcf10a_common_project/mcf10a_atacseq/archive/Data/rdata/dob_new.Rdata"
+annoFile  <- "../ATACseq/Metadata/MDD_ATACseq_sampleMetadata.csv"
+RNAfile <- "../RNAseq/Data/MDD_RNAseq_Level3.csv"
+
+# out
+motifFamilies_out <- "../ATACseq/Data/motif/MDD_ATACseq_MotifFamilyScores.csv"
+motifScores_out   <- "../ATACseq/Data/motif/MDD_ATACseq_MotifScores.csv"
+motifAnno_out     <- "../ATACseq/Data/motif/MDD_ATACseq_MotifAnno_orig.csv"
+
+###############################################################################
+source("MDD_ATACseq_motifFunctions.R")
+###############################################################################
+# Loading ATACseq data
+load(rdata)
+
+# Loading motifs
+motifs <- jaspar()
+
+# Sample annotation file
+sampleAnno <- 
+  read.csv(annoFile, stringsAsFactors = FALSE) %>% 
+  mutate(ligand = fct_inorder(ligand)) %>% 
+  mutate(experimentalCondition = fct_inorder(experimentalCondition)) %>% 
+  filter(ATACseq_QCpass)
+
+# annotation table to map uniprot ids to ensembl gene ids/hugo symbols
+mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                dataset = "hsapiens_gene_ensembl")
+
+annoTable <-
+  getBM(attributes = c("ensembl_gene_id", "hgnc_symbol",
+                       "uniprot_gn_id"),
+        mart = mart)
+
+##############################################################################
+# Calculating motif scores
+motifScores <- calculateMotifScores(dob.counts, motifs, sampleAnno) %>% 
+  data.frame %>% 
+  rownames_to_column("motif")
+
+##############################################################################
+# Creating motif annotations
+# Motif families
+mFam <-
+  lapply(motifs@listData, function(x) {
+    fam <- x@tags$family
+    if(anyNA(fam)) {
+      fam <- "None"
+      }
+    return(c(fam))
+    }) %>% 
+  stack
+colnames(mFam) <- c("family", "motif")
+
+# UniProt accession #
+mAcc <-
+  lapply(motifs@listData, function(x) {
+    uniprot <- x@tags$acc
+    if(anyNA(uniprot)) {
+      uniprot <- "None"
+    }
+    return(c(uniprot))
+  }) %>% 
+  stack
+colnames(mAcc) <- c("uniprot_gn_id", "motif")
+
+motifAnno <- 
+  mAcc %>% 
+  left_join(mFam) %>% 
+  dplyr::select(motif, family, uniprot_gn_id) %>% 
+  left_join(annoTable)
+
+write_csv(motifScores,
+          path = motifScores_out)
+
+write_csv(motifAnno, 
+          path = motifAnno_out)
+
+##############################################################################
+# Summarizing motifs to families
+motif_families <-
+  motifAnno %>% 
+  group_by(motif) %>% 
+  left_join(motifAnno) %>% 
+  dplyr::select(-uniprot_gn_id) %>%
+  filter(!(grepl("var", motif))) %>% 
+  mutate(family = str_trim(family, "right")) %>%  # cleaning names
+  mutate(family = str_replace(family, "ü", "u")) %>% 
+  left_join(motifScores) %>% 
+  pivot_longer(contains("sid"), values_to = "motifEnrichment", names_to = "specimenID") %>% 
+  group_by(motif) %>% 
+  mutate(motifVar = var(motifEnrichment)) %>% 
+  filter(!is.na(family)) %>%
+  group_by(family, specimenID) %>% 
+  summarize(motifEnrichment = mean(motifEnrichment)) %>% 
+  data.frame() %>% 
+  pivot_wider(names_from = specimenID,
+              values_from = motifEnrichment)
+
+motif_families <- motif_families[, c("family", sampleAnno %>% filter(ATACseq_QCpass) %>% pull(specimenID))]
+write_csv(motif_families, path = motifFamilies_out)
+
+##########
+
+# apply(column_to_rownames(motifScores, "motif"), 1, function(x) {X <- var(x)^(1/2); X}) %>% 
+#   summary
+# apply(column_to_rownames(motif_families, "family"), 1, function(x) {X <- var(x)^(1/2); X}) %>% 
+#   summary
+
---
+++ R/MDD_ATACseq_motifFunctions.R
@@ -0,0 +1,73 @@
+calculateMotifScores <- function(countsDob, referenceMotifs, colData, expect = TRUE) {
+  
+  atacseqCounts <- 
+    dba.peakset(countsDob, bRetrieve = TRUE) %>% 
+    data.frame()
+  
+  atacseqCounts <- as.matrix(atacseqCounts[, colData$specimenID])
+  
+  atacseqRowRanges <- 
+    dba.peakset(countsDob, bRetrieve = TRUE)
+  
+  # Creating a RangedSummariedExperiment, adding GC bias, filtering peaks
+  atacRSE <-
+    SummarizedExperiment(assays = list(counts = atacseqCounts),
+                         rowRanges = atacseqRowRanges,
+                         colData = colData)
+  
+  atacRSE <- addGCBias(atacRSE, genome = BSgenome.Hsapiens.UCSC.hg38)
+  atacRSE <- filterPeaks(atacRSE, non_overlapping = TRUE)
+  
+  # Finding motifs in peaks, computing accessibliity deviations
+  motifsInPeaks <- matchMotifs(referenceMotifs, atacRSE,
+                               genome = BSgenome.Hsapiens.UCSC.hg38)
+  
+  if(expect) {
+    expected <- computeExpectations(atacRSE, norm = TRUE)
+    motifDev <-
+      computeDeviations(atacRSE,
+                        annotations = motifsInPeaks,
+                        expectation = expected)
+  } else {
+    motifDev <-
+      computeDeviations(atacRSE,
+                        annotations = motifsInPeaks)
+  }
+  
+  # getting raw scores of deviations
+  motifScores <- deviationScores(motifDev)
+  
+  return(motifScores)
+}
+
+collapseReps <- function(x, sA) {
+  mat_medians <- sapply(unique(sA$experimentalCondition), function(X) {
+    Xids <-
+      sA %>% 
+      filter(experimentalCondition == X) %>% 
+      pull(specimenID)
+    Xmat <- x[, Xids]    # matrix of one condition's replicates
+    if (length(Xids) > 1) {
+      Xmat_median <- apply(Xmat, 1, median)
+    } else {
+      Xmat_median <- Xmat
+    }
+    return(Xmat_median)
+  })
+  colnames(mat_medians) <- unique(sA$experimentalCondition)
+  mat_medians <- data.frame(mat_medians)
+  return(mat_medians)
+}
+
+jaspar <- function (...) 
+{
+  opts <- list()
+  opts["species"] <- "Homo sapiens"
+  opts["collection"] <- "CORE"
+  opts <- c(opts, list(...))
+  out <- TFBSTools::getMatrixSet(JASPAR2018::JASPAR2018, opts)
+  if (!isTRUE(all.equal(TFBSTools::name(out), names(out)))) 
+    names(out) <- paste(names(out), TFBSTools::name(out), 
+                        sep = "_")
+  return(out)
+}
